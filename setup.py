dbbb53e89886c7fd59826e8551f79a697ea51dcf
amrotork <amtc2018@gmail.com>
2017-10-20 14:51:15 +0200

    - Adding setup script to support it as a package.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='MP_Pandas',
+    version='0.1',
+    author='josepm,amro-pydev',
+    author_email='amrsfmt@yahoo.com',
+    maintainer='Amro Tork',
+    maintainer_email='amrsfmt@yahoo.com',
+    url='https://github.com/amro-pydev/MP_Pandas.git',
+    license="BSD",
+    description='Pandas Multiprocessing Support',
+    long_description='Python Multiprocesing Support',
+    packages=['MP_Pandas'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Scientific/Engineering'
+    ]
+)
