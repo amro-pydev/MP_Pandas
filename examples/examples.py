48ca1b23712310b70d86b1d1c9687929d22f4b2b
amrotork <amtc2018@gmail.com>
2017-10-20 15:25:23 +0200

    - Simplify examples.

--- examples/examples.py
+++ examples/examples.py
@@ -2,25 +2,9 @@ import pandas as pd
 import numpy as np
 import sys
 import os
-home = os.path.expanduser("~")
-sys.path.append(home + '/code')
 import MP_Pandas as mp
 from datetime import datetime
-import pytz
 import time
-
-EPOCH = datetime(1970, 1, 1, 0, 0, 0, tzinfo=pytz.utc)
-
-
-def time_now(epoch=EPOCH):
-    tt = datetime.utcnow().replace(tzinfo=pytz.utc)
-    return (tt - epoch).total_seconds()
-
-
-def timing(t0):
-    t1 = time_now()
-    return t1, t1 - t0
-
 
 def apply_func(df, wait, work):
     time.sleep(wait)
@@ -39,13 +23,13 @@ df = pd.DataFrame({'gb1': np.random.randint(low=0, high=high_1, size=df_len),
 gb_cols = ['gb1', 'gb2']
 args = [0.01, 0.001]  # wait, work
 
-t0 = time_now()
+t0 = time.time()
 z = df.groupby(gb_cols).apply(apply_func, *args)
-print(timing(t0))
+print("## Single thread run time {:f} seconds.".format(time.time() - t0))
 
 
 mp_args = {'n_cpus': 6, 'queue': True, 'n_queues': None}
 
-t0 = time_now()
+t0 = time.time()
 z = mp.mp_groupby(df, gb_cols, apply_func, *args, **mp_args)
-print(timing(t0))
+print("## Parallel run time {:f} seconds.".format(time.time() - t0))
